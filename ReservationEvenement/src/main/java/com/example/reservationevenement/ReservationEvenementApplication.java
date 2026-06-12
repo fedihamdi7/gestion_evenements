75e56eb0a259dfbaa3bddc1175c9e73a58ee52fa
AbderrahimBELHAJ <AbderrahimBELHAJ@DESKTOP-LDCTIH6>
2026-06-12 16:43:49 +0100

    Ajout microservice reservationevents

---
+++ ReservationEvenement/src/main/java/com/example/reservationevenement/ReservationEvenementApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservationevenement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationEvenementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationEvenementApplication.class, args);
+    }
+
+}
---
+++ ReservationEvenement/src/test/java/com/example/reservationevenement/ReservationEvenementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservationevenement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationEvenementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
